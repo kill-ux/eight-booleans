c8cadcc71fafcbf22158e1818b55ce8d5e179bbd
killux <mustaphaboutoubdev@gmail.com>
2025-12-10 16:43:09 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -39,3 +39,40 @@ impl From<u8> for Index {
         Index::new_unchecked(item)
     }
 }
+
+
+// READ | CLEAR | SET | TOGGLE
+#[derive(Clone, Copy)]
+pub struct BitWise {
+    value: u8,
+}
+
+impl BitWise {
+    pub fn new() -> Self {
+        BitWise { value: 0 }
+    }
+
+    pub fn clear(&mut self, index: Index) {
+        self.value &= !(1u8 << index.get());
+    }
+
+    pub fn read(self, index: Index) -> bool {
+        self.value >> index.get() & 1 == 1
+    }
+
+    pub fn set(&mut self, index: Index, new_value: bool) {
+        if new_value == true {
+            self.value = self.value | (1 << index.get());
+        } else {
+            self.value = self.value & !(1 << index.get());
+        }
+    }
+
+    pub fn toggle(&mut self, index: Index) {
+        self.value ^= 1u8 << index.get();
+    }
+
+    pub fn desplay(self) {
+        println!("{:08b}", self.value);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,27 @@
-use bitwise_operators::Index;
-
-// READ | CLEAR | SET | TOGGLE
-#[derive(Clone, Copy)]
-struct BitWise {
-    value: u8,
-}
-
-impl BitWise {
-    pub fn new() -> Self {
-        BitWise { value: 0 }
-    }
-
-    pub fn clear(&mut self, index: Index) {
-        self.value &= !(1u8 << index.get());
-    }
-  
-    pub fn read(self,index: Index) -> bool {
-        self.value >> index.get() & 1 == 1
-    }
-
-    pub fn set(&mut self,index: Index, new_value: bool ) {
-        if new_value == true{
-            self.value = self.value | (1 << index.get());
-        }else{
-            self.value = self.value & !(1 << index.get());
-        }
-    }
-
-    pub fn toggle(&mut self, index: Index) {
-        self.value ^= 1u8 << index.get();
-    }
-}
+use bitwise_operators::BitWise;
 
 fn main() {
-    let mut bol_example = BitWise::new();
-    
-    let bit = BitWise { value: 1 };
+    let mut booleans = BitWise::new();
+
+    println!("###### Set #######");
+    booleans.desplay();
+    booleans.set(1.into(), true);
+    booleans.desplay();
+    println!("####################");
+
     for i in 0..=7 {
-        println!(" index {i} = {}", bit.read(i.into()).unwrap());
+        println!(" index {i} = {}", booleans.read(i.into()));
     }
 
     println!("###### Toggle ######");
-    println!("{:08b}", bol_example.value);
-    bol_example.toggle(1.into());
-    println!("{:08b}", bol_example.value);
+    booleans.desplay();
+    booleans.toggle(1.into());
+    booleans.desplay();
     println!("####################");
 
     println!("###### Clear ######");
-    println!("{:08b}", bol_example.value);
-    bol_example.clear(1.into());
-    println!("{:08b}", bol_example.value);
+    booleans.desplay();
+    booleans.clear(1.into());
+    booleans.desplay();
     println!("####################");
 }
